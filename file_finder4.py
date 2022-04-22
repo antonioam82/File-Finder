24b9c7aa44594c99b41ec724b2ef2650b3c3fbb9
Antonio Alfonso <antonioalfonsomartinez255@gmail.com>
2022-04-22 15:42:20 +0200

    Se definió la función 'start()' para mostrar título y el directorio actual. Se definió la variable 'command' para definir la acción a realizar. Se eliminó la cariable 'conti'.

--- file_finder4.py
+++ file_finder4.py
@@ -17,6 +17,7 @@ def change_dir():
         dire = input("Introduzca directorio base: ").strip()
         if os.path.isdir(dire):
             os.chdir(dire)
+            print("Directorio actual: {} ".format(os.getcwd()))
             break
         else:
             print(Fore.RED+"ERROR, DIRECTORIO NO VÁLIDO"+Fore.RESET)
@@ -27,45 +28,50 @@ def show_dir(direc):
         print(Fore.BLUE+Back.WHITE+direc+Fore.RESET+Back.RESET)
         showed_dir = True
 
-conti = "s"
-init()
-
-command_list = ['cl','cbd','sch','q']#lista comandos
-
-while conti.lower() == "s":
+def start():
     print(Back.BLUE+"\n--------------------------FILE FINDER WITH REGEX--------------------------")
     print(Back.RESET+"")
     print("Directorio actual: {} ".format(os.getcwd()))
+    
+init()
+
+command_list = ['cl','cbd','sch','q']#lista comandos
+start()
+while True:
+    
     count = 0
     showed_dir = False
-					
-    change_dir()
-    texto_entrada = BMP(input("Introduce patrón de búsqueda: "))
-    print("BUSCANDO...\n")
-    try:
-        for root, folders, files in os.walk(os.getcwd()):
-            for file in files:
-                match_ = re.search(texto_entrada, file)
-                if match_:
-                    show_dir(root)
-                    count+=1
-                    print(Fore.GREEN+'{}-'.format(count)+os.path.join(root,BMP(Fore.YELLOW+Style.DIM+file+Fore.RESET+Style.NORMAL)))
-            showed_dir = False
-            
-            
-        if count == 0:
-            print(Fore.BLACK+Back.RED+"No se encontraron coincidencias con \'{}\'.".format(texto_entrada))
-        else:
-            if count == 1:
-                print(Fore.BLACK+Back.GREEN+"\n1 ARCHIVO ENCONTRADO.")
-            else:
-                print(Fore.BLACK+Back.GREEN+"\n{} ARCHIVOS ENCONTRADOS.".format(count))
-                
-    except Exception as e:
-        print(Fore.BLACK+Back.RED+'ERROR: {} '.format(str(e)))
-        
-    print(Fore.RESET+Back.RESET+"")
-	
-    conti = ns(input("¿Continuar(n/s)?: "))
-
+    command = input("Command: ")
     
+    if command in command_list:
+        if command == "cbd":
+            change_dir()
+        elif command == "q":
+            break
+        elif command == "sch":
+            texto_entrada = BMP(input("Introduce patrón de búsqueda: "))
+            print("BUSCANDO...\n")
+            try:
+                for root, folders, files in os.walk(os.getcwd()):
+                    for file in files:
+                        match_ = re.search(texto_entrada, file)
+                        if match_:
+                            show_dir(root)
+                            count+=1
+                            print(Fore.GREEN+'{}-'.format(count)+os.path.join(root,BMP(Fore.YELLOW+Style.DIM+file+Fore.RESET+Style.NORMAL)))
+                    showed_dir = False
+                if count == 0:
+                    print(Fore.BLACK+Back.RED+"No se encontraron coincidencias con \'{}\'.".format(texto_entrada))
+                else:
+                    if count == 1:
+                        print(Fore.BLACK+Back.GREEN+"\n1 ARCHIVO ENCONTRADO.")
+                    else:
+                        print(Fore.BLACK+Back.GREEN+"\n{} ARCHIVOS ENCONTRADOS.".format(count))
+                
+            except Exception as e:
+                print(Fore.BLACK+Back.RED+'ERROR: {} '.format(str(e)))
+                
+        print(Fore.RESET+Back.RESET+"")
+        
+    else:
+        print(Fore.RED+"ERROR, COMANDO NO VÁLIDO"+Fore.RESET)    
